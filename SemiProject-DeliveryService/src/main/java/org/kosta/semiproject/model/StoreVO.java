9869013ee6505cc22302b45c752dcb4b4d4ff0aa
shj22k <shj22k@naver.com>
2023-06-22 14:29:17 +0900

    사진 추가, storevo 추가

---
+++ SemiProject-DeliveryService/src/main/java/org/kosta/semiproject/model/StoreVO.java
@@ -0,0 +1,57 @@
+package org.kosta.semiproject.model;
+
+public class StoreVO {
+	private long storeNumber;
+	private String storeName;
+	private String storeLocation;
+	private String storeCategory;
+	private String storePhoneNumber;
+	public StoreVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public StoreVO(long storeNumber, String storeName, String storeLocation, String storeCategory,
+			String storePhoneNumber) {
+		super();
+		this.storeNumber = storeNumber;
+		this.storeName = storeName;
+		this.storeLocation = storeLocation;
+		this.storeCategory = storeCategory;
+		this.storePhoneNumber = storePhoneNumber;
+	}
+	public long getStoreNumber() {
+		return storeNumber;
+	}
+	public void setStoreNumber(long storeNumber) {
+		this.storeNumber = storeNumber;
+	}
+	public String getStoreName() {
+		return storeName;
+	}
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	public String getStoreLocation() {
+		return storeLocation;
+	}
+	public void setStoreLocation(String storeLocation) {
+		this.storeLocation = storeLocation;
+	}
+	public String getStoreCategory() {
+		return storeCategory;
+	}
+	public void setStoreCategory(String storeCategory) {
+		this.storeCategory = storeCategory;
+	}
+	public String getStorePhoneNumber() {
+		return storePhoneNumber;
+	}
+	public void setStorePhoneNumber(String storePhoneNumber) {
+		this.storePhoneNumber = storePhoneNumber;
+	}
+	@Override
+	public String toString() {
+		return "StoreVO [storeNumber=" + storeNumber + ", storeName=" + storeName + ", storeLocation=" + storeLocation
+				+ ", storeCategory=" + storeCategory + ", storePhoneNumber=" + storePhoneNumber + "]";
+	}
+}
